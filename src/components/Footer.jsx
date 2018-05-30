3c09431c94f7ce740f03978f16ac302dcea9f8da
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-05-29 21:36:12 -0300

    feat: Add basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import logo from '../feesimple-logo.svg'
+import {
+    Container
+} from 'reactstrap'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="fsfooter">
+        <Container>
+          <img src={logo} className="fsfooter-logo" alt="logo" href="/"/>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default Footer
