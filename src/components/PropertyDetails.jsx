0e14bf189ba9e8660250a071eda05b80cece3f4c
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-06-03 17:52:00 -0300

    fix: Remove reduntant component

--- src/components/PropertyDetails.jsx
+++ src/components/PropertyDetails.jsx
@@ -9,7 +9,29 @@ import {
 } from 'reactstrap'
 
 class PropertyDetails extends Component {
+  state = {
+    isEditing: false
+  }
+
+  toggleEdit = (e) => {
+    e.preventDefault()
+
+    const {isEditing} = this.state
+    this.setState({isEditing: !isEditing})
+  }
+
+  cancel = (e) => {
+    this.toggleEdit(e)
+    console.info('Cancel edit')
+  }
+
+  save = (e) => {
+    console.info('Save changes')
+  }
+
   render() {
+    const {isEditing} = this.state
+
     return (
       <div>
         <h1>Property Details</h1>
@@ -18,48 +40,60 @@ class PropertyDetails extends Component {
           <FormGroup row>
             <Label for="propertyName" sm={2}>Name</Label>
             <Col sm={10}>
-             <Input name="propertyName" id="propertyName" disabled/>
+             <Input name="propertyName" id="propertyName" disabled={!isEditing}/>
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="address1" sm={2}>Address 1</Label>
             <Col sm={10}>
-              <Input type="textarea" name="address1" id="address1" disabled />
+              <Input type="textarea" name="address1"
+                id="address1" disabled={!isEditing} />
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="address1" sm={2}>Address 2</Label>
             <Col sm={10}>
-              <Input type="textarea" name="address2" id="address2" disabled />
+              <Input type="textarea" name="address2"
+                id="address2" disabled={!isEditing} />
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="city" sm={2}>City</Label>
             <Col sm={10}>
-             <Input name="city" id="city" disabled />
+             <Input name="city" id="city" disabled={!isEditing} />
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="region" sm={2}>Region</Label>
             <Col sm={10}>
-             <Input name="region" id="region" disabled/>
+             <Input name="region" id="region" disabled={!isEditing}/>
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="postalCode" sm={2}>Postal Code</Label>
             <Col sm={10}>
-             <Input name="postalCode" id="postalCode" disabled />
+             <Input name="postalCode" id="postalCode" disabled={!isEditing} />
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="unitCount" sm={2}>Unit Count</Label>
             <Col sm={10}>
-             <Input type="number" name="unitCount" id="unitCount" disabled/>
+             <Input type="number" name="unitCount"
+              id="unitCount" disabled={!isEditing}/>
             </Col>
           </FormGroup>
-          <FormGroup check row>
-            <Button color="primary">Edit</Button>
-          </FormGroup>
+          <Button color="primary" hidden={isEditing}
+            onClick={(e) => this.toggleEdit(e)}>
+            Edit
+          </Button>
+          <Button color="primary" hidden={!isEditing}
+            onClick={(e) => this.save(e)} style={{'margin-right':'0.5em'}}>
+            Save
+          </Button>
+          <Button outline color="primary" hidden={!isEditing}
+            onClick={(e) => this.cancel(e)}>
+            Cancel
+          </Button>
         </Form>
       </div>
     )
