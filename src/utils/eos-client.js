d24c17332ce7cbccf24984dc7e25bea788fb5be0
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-07-16 21:47:19 -0300

    fix: Update key and refactor

--- src/utils/eos-client.js
+++ src/utils/eos-client.js
@@ -1,19 +1,19 @@
 import eosjs from 'eosjs2'
 
-const rpc = new eosjs.Rpc.JsonRpc('http://127.0.0.1:8877');
-const signatureProvider = new eosjs.SignatureProvider(['5JxDmFneCLdS2LKqBacTpHdFRvaBXQs9m956R3ovCohXCdFvPma']);
+const rpc = new eosjs.Rpc.JsonRpc('http://127.0.0.1:8877')
+const signatureProvider = new eosjs.SignatureProvider(['5KdUmNF1T6aNhzohopYNFL5p2JQq9op6TwXXtB6pvkwWSkyB53w'])
 
 export default class EOSClient {
   constructor(contractName, contractSender) {
-    this.contractName = contractName;
-    this.contractSender = contractSender;
+    this.contractName = contractName
+    this.contractSender = contractSender
 
     this.eos = new eosjs.Api({ rpc, signatureProvider })
   }
 
   getTableRows = table => {
-    return rpc.get_table_rows({json: true, code: this.contractSender, scope: this.contractName, table});
-  };
+    return rpc.get_table_rows({json: true, code: this.contractSender, scope: this.contractName, table})
+  }
 
   transaction = (action, data) => {
     return this.eos.pushTransaction({
@@ -34,6 +34,6 @@ export default class EOSClient {
           }
         }
       ]
-    });
-  };
+    })
+  }
 }
