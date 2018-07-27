9794846f473a506b48d7e90437dc4b137cb33101
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-07-27 00:23:24 -0300

    feat: Add getAccountFrom method to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,20 @@ export const idFromPath = (pathname) => {
   const result = pathname.substring(pathname.lastIndexOf('/') + 1, pathname.length)
   return result === '' ? '-1' : result
 }
+
+export const getAccountFrom = async (scatter, network) => {
+  if(!scatter){
+    console.error('No scatter available.')
+    throw new Error(500)
+  }
+
+  if(!scatter.identity || !scatter.identity.accounts[0]){
+    console.info('No identity available. Requesting...')
+    await scatter.getIdentity({ accounts: [network] })
+  }
+
+  return scatter
+    .identity
+    .accounts
+    .find(account => account.blockchain === 'eos')
+}
