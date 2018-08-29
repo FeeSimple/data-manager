9dbd984a6e46a91989b9e0f3a125fffe99f50757
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-08-29 17:45:15 -0300

    fix: Add missing reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { properties } from './properties'
 import { scatter } from './scatter'
 import { eosClient } from './eosclient'
 import { contracts } from './contracts'
+import { accountData } from './accountData'
 
 export default combineReducers({
   properties,
   eosClient,
   scatter,
-  contracts
+  contracts,
+  accountData
 })
