ba235f088ad3776f11d488ea43e8a07bf49d187e
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-10-01 14:14:57 -0300

    fix: sintax error

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,15 +3,15 @@ import OpenReLogoBlack from '../../img/openre-logo-black.svg'
 
 export default function Footer () {
   return (
-    <div class="fs-footer">
-      <span class="badge badge-pill badge-fs bold" style={{color: 'white'}}>
+    <div className="fs-footer">
+      <span className="badge badge-pill badge-fs bold" style={{color: 'white'}}>
         v0.01
       </span>
       {' '}
       Part of the FeeSimple network.
-      <span class="created-by">
+      <span className="created-by">
         {' '}Crafted by
-        <a href="" class="p-l-10">
+        <a href="" className="p-l-10">
           <img src={OpenReLogoBlack} alt="logo" width="110"/>
         </a>
       </span>
