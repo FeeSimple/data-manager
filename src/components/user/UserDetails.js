4c587d178eb0254541b8268227988897cb8ec848
trung <>
2018-10-19 18:44:11 +0700

    Design the HTML view of user profile.

--- src/components/user/UserDetails.js
+++ src/components/user/UserDetails.js
@@ -7,7 +7,7 @@ export const EDITING = 'editing'
 export const CREATING = 'creating'
 
 const UserDetails = ({
-  property,
+  user,
   mode,
   onEditClick,
   onCreateClick,
@@ -19,142 +19,68 @@ const UserDetails = ({
       <Container>
         <Row>
           <Col>
-            <h3 className="float-left">New Property</h3>
+            <h3 className="float-left">User Account Information</h3>
           </Col>
         </Row>
       </Container>
     </div>
     <br />
     <Container>
-      <form className="">
-        <div className="form-group row">
-          <div className="col-12">
-            <h3 className="bar-header">Details</h3>
-          </div>
+      <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
+        <Row>
+          <Col>
+            <h3 className="user-detail-label">Name</h3>
+          </Col>
+          <Col>
+            <h3 className="user-detail-label" id="account-name-lookup">Trung</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="user-detail-label">Creation date</h3>
+          </Col>
+          <Col>
+            <h3 
+              className="user-detail-label" id="creation-lookup">Trung</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="user-detail-label">Balance</h3>
+          </Col>
+          <Col>
+            <h3 
+              className="user-detail-label" id="balance-lookup">Trung</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="user-detail-label">RAM</h3>
+          </Col>
+          <Col>
+            <meter id="ram-lookup-meter" style={{width: 200}} low="0.5" optimum="0.4" high="0.7" value="0.75"></meter>
+            <b className="user-detail-label" id="ram-lookup-text">75%</b>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 className="user-detail-label">CPU</h3>
+          </Col>
+          <Col>
+            <meter id="cpu-lookup-meter" style={{width: 200}} low="0.5" optimum="0.4" high="0.7" value="0.6"></meter>
+            <b className="user-detail-label" id="cpu-lookup-text">60%</b>
+          </Col>
+        </Row>
+        <Row>
+            <Col>
+              <h3 className="user-detail-label">Bandwidth</h3>
+            </Col>
+            <Col>
+              <meter id="bandwidth-lookup-meter" style={{width: 200}} low="0.5" optimum="0.4" high="0.7" value="0.5"></meter>
+              <b className="user-detail-label" id="bandwidth-lookup-text">50%</b>
+            </Col>
+          </Row>
         </div>
-        <div className="form-group row">
-          <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-            <label className="form-label">Name</label>
-            <input
-              id='name'
-              type="text"
-              className="form-control"
-              name='name'
-              value={property.name}
-              onChange={onChange}
-              disabled={mode === READING} />
-          </div>
-        </div>
-        <div className="form-group row">
-          <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-            <label className="form-label">Address 1</label>
-            <input
-              id='address_1'
-              className="form-control"
-              placeholder=""
-              name='address_1'
-              type='textarea'
-              onChange={onChange}
-              value={property.address_1}
-              disabled={mode === READING}
-              />
-          </div>
-        </div>
-        <div className="form-group row">
-          <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-            <label className="form-label">Address 2</label>
-            <input
-              id='address_2'
-              className="form-control"
-              name='address_2'
-              type='textarea'
-              value={property.address_2}
-              onChange={onChange}
-              disabled={mode === READING}
-              />
-          </div>
-        </div>
-        <div className="form-group row">
-          <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-            <label className="form-label">City</label>
-            <input
-              id='city'
-              className="form-control"
-              name='city'
-              type="text"
-              disabled={mode === READING}
-              onChange={onChange}
-              value={property.city}
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-            <label className="form-label">Postal Code</label>
-            <input
-              id='postal_code'
-              name='postal_code'
-              className="form-control"
-              type="text"
-              disabled={mode === READING}
-              onChange={onChange}
-              value={property.postal_code}
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <div className="col-12 col-md-10 col-lg-8 offset-md-1 offset-lg-2">
-            <label className="form-label">Unit Count</label>
-            <input
-              id='unit_count'
-              name='unit_count'
-              className="form-control"
-              type="text"
-              disabled={mode === READING}
-              onChange={onChange}
-              value={property.unit_count}
-            />
-          </div>
-        </div>
-        <div className="form-group m-t-50 row">
-          <div className="col-md-5 col-lg-4 offset-md-1 offset-lg-2 col-6">
-            <button
-              type="button"
-              className="btn btn-base w100 form-btn"
-              hidden={mode !== READING}
-              onClick={(e) => onEditClick(e, property)}
-            >
-              Edit
-            </button>
-            <button
-              type="button"
-              className="btn btn-base w100 form-btn"
-              hidden={mode !== EDITING}
-              onClick={(e) => onSaveClick(e)} style={{marginRight: '0.5em'}}
-            >
-              Save
-            </button>
-            <button
-              type="button"
-              className="btn btn-base w100 form-btn"
-              hidden={mode !== CREATING}
-              onClick={(e) => onCreateClick(e, property)}
-            >
-              Create
-            </button>
-          </div>
-          <div className="col-md-5 col-lg-4 col-6">
-            <Link to="/">
-              <button
-                type="button"
-                className="btn btn-gray-o w100 form-btn"
-              >
-                Cancel
-              </button>
-            </Link>
-          </div>
-        </div>
-      </form>
     </Container>
   </div>
 )
