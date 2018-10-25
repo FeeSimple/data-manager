e02a353818e438c42b929cfb93fb2716f87245d3
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-10-24 21:21:32 -0300

    fix: Minor warnings and style

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ function ensureSecure (req, res, next) {
   if (req.secure) {
     // OK, continue
     return next()
-  };
+  }
   // handle port numbers if you need non defaults
-  // res.redirect('https://' + req.host + req.url); // express 3.x
+  // res.redirect('https://' + req.host + req.url) // express 3.x
   res.redirect('https://' + req.hostname + req.url) // express 4.x
 }
--- src/components/user/index.js
+++ src/components/user/index.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { beautifyRam, getResourceStr } from '../../actions'
+import { getResourceStr } from '../../actions'
 import UserDetails from './UserDetails'
 
 class UserDetailsContainer extends Component {
   constructor() {
-    super();
-    this.state = {data: []};
+    super()
+    this.state = {data: []}
   }
 
   componentDidMount() {
     const { eosClient, accountData } = this.props
     let account = accountData.active
     eosClient.getAccount(account).then(result => {
-      const ramStr = getResourceStr({used: result.ram_usage, max: result.ram_quota});
-      const ramMeter = (new Intl.NumberFormat().format(1-(result.ram_usage / result.ram_quota)).toString());
-      
-      const bandwidthStr = getResourceStr(result.net_limit);
-      const bandwidthMeter = (new Intl.NumberFormat().format(1-(result.net_limit.used / result.net_limit.max)).toString());
-      
-      const cpuStr = getResourceStr(result.cpu_limit, true);
-      const cpuMeter = (new Intl.NumberFormat().format(1-(result.cpu_limit.used / result.cpu_limit.max)).toString());
+      const ramStr = getResourceStr({used: result.ram_usage, max: result.ram_quota})
+      const ramMeter = (new Intl.NumberFormat().format(1-(result.ram_usage / result.ram_quota)).toString())
+
+      const bandwidthStr = getResourceStr(result.net_limit)
+      const bandwidthMeter = (new Intl.NumberFormat().format(1-(result.net_limit.used / result.net_limit.max)).toString())
+
+      const cpuStr = getResourceStr(result.cpu_limit, true)
+      const cpuMeter = (new Intl.NumberFormat().format(1-(result.cpu_limit.used / result.cpu_limit.max)).toString())
 
       const balance = result.core_liquid_balance
       const created = result.created
       const pubkey = result.permissions[0].required_auth.keys[0].key
-      
+
       const info = {
         account,
         created,
