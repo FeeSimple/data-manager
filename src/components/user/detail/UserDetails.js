e2ee60bbc0709b8cb77a95ff8fda8367907f9c3a
trung <>
2018-11-01 22:09:54 +0700

    Design buttons "Account", "Resource Management", "Send", "Receive" and "Activity" buttons in the account view

--- src/components/user/detail/UserDetails.js
+++ src/components/user/detail/UserDetails.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
-
+import { Link } from 'react-router-dom'
 const meterControl = 'low="0.5" optimum="0.6" high="0.3"'
 
 const UserDetails = ({
@@ -11,7 +11,29 @@ const UserDetails = ({
       <Container>
         <Row>
           <Col>
-            <h3 className="float-left">Account</h3>
+            <span className="float-left">
+              <Link to="/account" className="btn btn-base-o prop-btn">Account</Link>
+            </span>
+          </Col>
+          <Col>
+            <span className="float-left">
+              <Link to="/account/resrc" className="btn btn-base-o prop-btn">Resource Management</Link>
+            </span>
+          </Col>
+          <Col>
+            <span className="float-right">
+              <Link to="/account/send" className="btn btn-base-o prop-btn">Send</Link>
+            </span>
+          </Col>
+          <Col>
+            <span className="float-right">
+              <Link to="/account/receive" className="btn btn-base-o prop-btn">Receive</Link>
+            </span>
+          </Col>
+          <Col>
+            <span className="float-right">
+              <Link to="/account/activity" className="btn btn-base-o prop-btn">Activity</Link>
+            </span>
           </Col>
         </Row>
       </Container>
