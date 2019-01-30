b7cfa52a735c10ce02f8888ae6d6aa9a494962ac
Matheus Alencar <mtsalenc@gmail.com>
2019-01-30 11:01:14 -0200

    fix: remove greenlock

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express')
+const http = require('http')
+const https = require('https')
 const path = require('path')
+const fs = require('fs')
+const privateKey  = fs.readFileSync('/etc/letsencrypt/live/fsmanager.io/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/fsmanager.io/fullchain.pem', 'utf8');
 
-const environment = process.env.ENVIRONMENT
-if(environment !== 'PRODUCTION' && environment !== 'TESTING') {
-  console.error('Error: .env variable ENVIRONMENT should be either PRODUCTION or TESTING')
-  process.exit()
-}
+const credentials = {key: privateKey, cert: certificate};
 
-const PROD = environment === 'PRODUCTION'? true : false
 const app = express()
 app.use(express.static('build'))
-app.get('/*', (req, res) => {
+app.get('/*', (undefined, res) => {
   res.sendFile(path.resolve('', 'build', 'index.html'));
 })
 
-require('greenlock-express').create({
-  version: 'draft-11',
-  server: PROD
-    ? 'https://acme-v02.api.letsencrypt.org/directory'
-    : 'https://acme-staging-v02.api.letsencrypt.org/directory',
-  configDir: '~/.config/acme/',
-  email: 'tim@feesimple.io',
-  approveDomains: [ 'fsmanager.io', 'www.fsmanager.io' ],
-  agreeTos: true,
-  app
-}).listen(80, 443)
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(credentials, app)
+
+httpServer.listen(80)
+httpsServer.listen(443)
+
+console.info('Server online')
