98e2a827ae988755bf15cb8a08e187a8d2a9e0fc
trung <>
2019-03-06 21:47:21 +0700

    Remove un-used things in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,68 +10,10 @@ import IconUser from '../../img/icon-user.svg'
 import IconLogout from '../../img/icon-logout.svg'
 import {
   setInfo,
-  setActive,
-  setFsMgrContract,
-  setEosClient
+  setActive
 } from '../../actions/index'
-import { beautifyRam } from '../../utils/beautify'
 
 class NavbarContainer extends Component {
-  constructor () {
-    super()
-    this.state = { data: [] }
-  }
-
-  async componentDidMount () {
-    const {
-      eosClient,
-      accountData,
-      setEosClient,
-      setFsMgrContract,
-      contracts
-    } = this.props
-
-    let account = accountData.active
-    let result = await eosClient.getAccount(account)
-    const created = result.created
-    const ram = result.ram_quota
-    const ramAvailable = beautifyRam(result.ram_quota - result.ram_usage)
-    const bandwidth = result.delegated_bandwidth
-    const pubkey = result.permissions[0].required_auth.keys[0].key
-    const info = {
-      account,
-      created,
-      ram,
-      ramAvailable,
-      bandwidth,
-      pubkey
-    }
-
-    // Store into redux
-    setInfo(info)
-
-    this.setState({ data: info })
-  }
-
-  beautifyRam (ram) {
-    let cnt = 0
-    while (cnt < 3 && ram >= 1024) {
-      ram = ram / 1024
-      cnt++
-    }
-    ram = new Intl.NumberFormat().format(ram)
-    if (cnt === 0) {
-      ram = ram.toString() + ' Byte'
-    } else if (cnt === 1) {
-      ram = ram.toString() + ' KB'
-    } else if (cnt === 2) {
-      ram = ram.toString() + ' MB'
-    } else if (cnt === 3) {
-      ram = ram.toString() + ' GB'
-    }
-
-    return ram
-  }
 
   handleLogout = () => {
     this.props.setActive(null)
@@ -125,15 +67,13 @@ class NavbarContainer extends Component {
   }
 }
 
-function mapStateToProps ({ eosClient, accountData, contracts }) {
-  return { eosClient, accountData, contracts }
+function mapStateToProps ({}) {
+  return {}
 }
 
 export default withRouter(
   connect(mapStateToProps, {
     setInfo,
-    setActive,
-    setFsMgrContract,
-    setEosClient
+    setActive
   })(NavbarContainer)
 )
