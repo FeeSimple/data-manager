0dec8173bdd85504153edde0a16aa918c6efc9ce
trung <>
2019-06-21 00:52:40 +0700

    Add "Copy" to clipboard buttons next to private and public key inputs during new account setup

--- src/components/account/NewAcc.js
+++ src/components/account/NewAcc.js
@@ -12,9 +12,12 @@ import {
   Label,
   Input,
   Alert,
-  Collapse
+  Collapse,
+  UncontrolledPopover, PopoverBody
 } from 'reactstrap'
 
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import {Icon} from 'antd';
 import { checkAccountNameError } from '../../utils/eoshelper'
 
 const NewAccForm = props => {
@@ -75,7 +78,13 @@ const NewAccForm = props => {
               </p>
             </Alert>
             <FormGroup>
-              <Label>Public Key</Label>
+              <Label>Public Key</Label> { }
+              <CopyToClipboard text={accountPubKey} id='copyPub'>
+                  <Icon type="copy" theme="twoTone" style={{ color: '#08c' }} />
+              </CopyToClipboard>
+              <UncontrolledPopover trigger="focus" placement="right" target="copyPub">
+                <PopoverBody>Public key copied to clipboard</PopoverBody>
+              </UncontrolledPopover>
               <Input
                 type='text'
                 id='accountPubKey'
@@ -84,7 +93,13 @@ const NewAccForm = props => {
               />
             </FormGroup>
             <FormGroup>
-              <Label>Private Key</Label>
+              <Label>Private Key</Label> { }
+              <CopyToClipboard text={accountPrivKey} id='copyPriv'>
+                  <Icon type="copy" theme="twoTone" style={{ color: '#08c' }} />
+              </CopyToClipboard>
+              <UncontrolledPopover trigger="focus" placement="right" target="copyPriv">
+                <PopoverBody>Private key copied to clipboard</PopoverBody>
+              </UncontrolledPopover>
               <Input
                 type='text'
                 id='accountPrivKey'
